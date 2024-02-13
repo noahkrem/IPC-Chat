66f8ca8627d343acced45bdd0bee5265e0d8ba83
noahkrem <kremlern@gmail.com>
2024-02-13 15:49:31 -0800

    Thread functions

--- IPC-Chat.c
+++ IPC-Chat.c
@@ -3,4 +3,26 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-// TEST
+// On receipt of input, adds the input to the list of messages
+//  to be sent to the remote s-talk client
+void * keyboard_thread () {
+
+}
+
+// Take each message off this list and send it over the network
+//  to the remote client
+void * UDP_output_thread() {
+
+}
+
+// On receipt of input from the remote s-talk client, puts the 
+//  message on the list of messages that need to be printed to
+//  the local screen
+void * UDP_input_thread() {
+
+}
+
+// Take each message off of the list and output to the screen
+void * screen_output_thread() {
+
+}
