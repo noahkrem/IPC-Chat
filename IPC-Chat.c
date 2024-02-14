fedcc79f67425ce39444329d93d211f03dc15f08
hugokwon5 <hugokwon5@gmail.com>
2024-02-13 17:04:08 -0800

    fixed makefile and added library

--- IPC-Chat.c
+++ IPC-Chat.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 //int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
 
